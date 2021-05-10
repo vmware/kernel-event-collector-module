8e77605b57ee4bfe16a722ab7ca888acc93dbd09
John Reiser <jreiser@carbonblack.com>
2021-05-10 14:19:01 +0000

    https://gitlab.bit9.local/psc/kernel-event-collector-module/-/merge_requests/88
    	modified:   project.yml
    	modified:   src/conanfile.py

--- src/task-helper.h
+++ src/task-helper.h
@@ -18,20 +18,20 @@ extern bool ec_task_initialize(ProcessContext *context);
 extern void ec_task_shutdown(ProcessContext *context);
 
 extern pid_t ec_getcurrentpid(void);
-extern pid_t ec_getpid(struct task_struct *task);
-extern pid_t ec_gettid(struct task_struct *task);
-extern pid_t ec_getppid(struct task_struct *task);
+extern pid_t ec_getpid(struct task_struct const *task);
+extern pid_t ec_gettid(struct task_struct const *task);
+extern pid_t ec_getppid(struct task_struct const *task);
 extern void ec_get_task_struct(struct task_struct *task);
 extern void ec_put_task_struct(struct task_struct *task);
 extern void ec_get_starttime(struct timespec *start_time);
 extern uint64_t ec_get_path_buffer_memory_usage(void);
-bool ec_task_get_path(struct task_struct *task, char *buffer, unsigned int buflen, char **pathname);
-extern bool ec_is_task_valid(struct task_struct *task);
-extern bool ec_is_task_alive(struct task_struct *task);
-struct task_struct *ec_find_task(pid_t pid);
-void ec_get_devinfo_from_task(struct task_struct *task, uint64_t *device, uint64_t *ino);
-struct inode *ec_get_inode_from_task(struct task_struct *task);
-bool ec_get_cmdline_from_binprm(struct linux_binprm *bprm, char *cmdLine, size_t cmdLineSize);
+bool ec_task_get_path(struct task_struct const *task, char *buffer, unsigned int buflen, char **pathname);
+extern bool ec_is_task_valid(struct task_struct const *task);
+extern bool ec_is_task_alive(struct task_struct const *task);
+struct task_struct const *ec_find_task(pid_t pid);
+void ec_get_devinfo_from_task(struct task_struct const *task, uint64_t *device, uint64_t *ino);
+struct inode const *ec_get_inode_from_task(struct task_struct const *task);
+bool ec_get_cmdline_from_binprm(struct linux_binprm const *bprm, char *cmdLine, size_t cmdLineSize);
 void ec_enumerate_and_track_all_tasks(ProcessContext *context);
 
 #define IS_CURRENT_TASK(a)   (strcmp(current->comm, (a)) == 0)
