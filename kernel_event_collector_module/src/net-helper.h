ab1c86ebf46519cf3dfe07ea55fbee0798d6d94a
Ben Smith <bsmith@carbonblack.com>
2021-11-18 16:33:58 -0500

    PSCLNX-9692 replace syscall network hooks with kprobes

--- kernel_event_collector_module/src/net-helper.h
+++ kernel_event_collector_module/src/net-helper.h
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "priv.h"
+
 #define CHECK_SK_FAMILY(sk)        ((sk) && \
                                      ((sk)->sk_family == PF_INET || (sk)->sk_family == PF_INET6) \
                                    )
@@ -32,6 +34,23 @@
 #define PROTOCOL_STR(PROTOCOL) ((PROTOCOL) == IPPROTO_TCP ? "tcp" : (PROTOCOL) == IPPROTO_UDP ? "udp" : "??")
 #define TYPE_STR(TYPE) ((TYPE) == SOCK_DGRAM ? "dgram" : (TYPE) == SOCK_STREAM ? "stream" : "??")
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 10, 0)
+#  define IPV4_SOCKNAME(inet, a) ((inet)->inet_##a)
+#  define IPV6_SOCKNAME(sk, a)   ((sk)->sk_v6_##a)
+#else
+#  define IPV4_SOCKNAME(inet, a) ((inet)->a)
+#  define IPV6_SOCKNAME(sk, a)   (inet6_sk(sk)->a)
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 0)
+#define ec_ipv6_skip_exthdr(skb, ptr, pProtocol) (ptr = ipv6_skip_exthdr(skb, ptr, pProtocol))
+#else
+#define ec_ipv6_skip_exthdr(skb, ptr, pProtocol) do {     \
+        __be16          frag_off;                               \
+        ptr = ipv6_skip_exthdr(skb, ptr, pProtocol, &frag_off); \
+    } while (0)
+#endif
+
 // ------------------------------------------------
 // Network Helpers
 //
@@ -40,8 +59,14 @@ void ec_set_sockaddr_port(CB_SOCK_ADDR *addr, uint32_t port);
 void ec_copy_sockaddr(CB_SOCK_ADDR *left, CB_SOCK_ADDR *right);
 void ec_copy_sockaddr_in(struct sockaddr_in *left, struct sockaddr_in *right);
 void ec_copy_sockaddr_in6(struct sockaddr_in6 *left, struct sockaddr_in6 *right);
-void ec_print_address(
+void __ec_print_address(
     char                  *msg,
     const struct sock     *sk,
     const struct sockaddr *localAddr,
     const struct sockaddr *remoteAddr);
+
+#define ec_print_address(msg, sk, localAddr, remoteAddr) do { if (MAY_TRACE_LEVEL(DL_NET)) __ec_print_address(msg, sk, localAddr, remoteAddr); } while (0)
+
+void ec_getsockname(struct sock *sk, CB_SOCK_ADDR *localAddr);
+bool ec_get_addrs_from_skb(struct sock *sk, struct sk_buff *skb, CB_SOCK_ADDR *srcAddr, CB_SOCK_ADDR *dstAddr);
+void ec_getpeername(struct sock *sk, CB_SOCK_ADDR *remoteAddr);
